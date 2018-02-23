273abddb3ae0f44ae79f25ffe17d94bf76a79d5f
Timothy Liu <tlkh@users.noreply.github.com>
2018-02-23 20:35:22 +0800

    organisation, automate filter testing

---
+++ run_filter_test.py
@@ -0,0 +1,142 @@
+import numpy as np
+import re, sys, os, csv, keras
+from nltk.tokenize import word_tokenize
+from keras import regularizers, initializers, optimizers, callbacks
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+from keras.layers import Embedding
+from keras.layers import Dense, Input, Flatten, Concatenate
+from keras.layers import Conv1D, MaxPooling1D, Embedding, Merge, Dropout, LSTM, GRU, Bidirectional
+from keras.models import Model
+from keras import backend as K
+from keras.engine.topology import Layer, InputSpec
+
+# setup
+filter_size = int(sys.argv[1])
+print("[!] Now performing test:",filter_size)
+
+MAX_NB_WORDS = 40000
+MAX_SEQUENCE_LENGTH = 30 # max length of text (words) minus pre-padding
+VALIDATION_SPLIT = 0.2
+EMBEDDING_DIM = 200 # for word2vec/GloVe
+GLOVE_DIR = "dataset/glove/glove.twitter.27B."+str(200)+"d.txt"
+print("[i] Loaded Parameters:\n",
+      MAX_NB_WORDS,MAX_SEQUENCE_LENGTH+5,
+      VALIDATION_SPLIT,EMBEDDING_DIM,"\n",
+      GLOVE_DIR)
+
+texts, labels = [], []
+print("[i] . Reading from csv file...", end="")
+with open('data.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        texts.append(row[0])
+        labels.append(row[1])
+print("Done!")
+
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+word_index = tokenizer.word_index
+print('[i] Found %s unique tokens.' % len(word_index))
+data_int = pad_sequences(sequences, padding='pre', maxlen=(MAX_SEQUENCE_LENGTH-5))
+data = pad_sequences(data_int, padding='post', maxlen=(MAX_SEQUENCE_LENGTH))
+
+labels = to_categorical(np.asarray(labels)) # convert to one-hot encoding vectors
+print('[+] Shape of data tensor:', data.shape)
+print('[+] Shape of label tensor:', labels.shape)
+
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
+
+print('[+] Number of entries in each category:')
+print("[+] Training:\n",y_train.sum(axis=0))
+print("[+] Validation:\n",y_val.sum(axis=0))
+
+embeddings_index = {}
+f = open(GLOVE_DIR)
+print("[i] Loading GloVe from:",GLOVE_DIR,"...",end="")
+for line in f:
+    values = line.split()
+    word = values[0]
+    embeddings_index[word] = np.asarray(values[1:], dtype='float32')
+f.close()
+print("Done.\n[+] Proceeding with Embedding Matrix...", end="")
+embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+print("Completed!")
+
+print("Finished running setup.")
+
+def get_lr_metric(optimizer):
+    def lr(y_true, y_pred):
+        return optimizer.lr
+    return lr
+
+def initial_boost(epoch):
+    if epoch==0: return float(6.0)
+    else: return float(1.0)
+
+def step_cyclic(epoch):
+    try:
+        l_r, decay = 1.0, 0.0001
+        if epoch%33==0:multiplier = 10
+        else:multiplier = 1
+        rate = float(multiplier * l_r * 1/(1 + decay * epoch))
+        #print("Epoch",epoch+1,"- learning_rate",rate)
+        return rate
+    except Exception as e:
+        print("Error in lr_schedule:",str(e))
+        return float(1.0)
+
+# actual experiment
+
+embedding_layer = Embedding(len(word_index) + 1,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=False)
+
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences = embedding_layer(sequence_input)
+l_lstm1 = Bidirectional(LSTM(4,dropout=0.2,recurrent_dropout=0.2,return_sequences=True))(embedded_sequences)
+l_cov1= Conv1D(24, filter_size, activation='relu')(l_lstm1)
+l_pool1 = MaxPooling1D(2)(l_cov1)
+l_drop1 = Dropout(0.3)(l_pool1)
+l_flat = Flatten()(l_drop1)
+l_dense = Dense(16, activation='relu')(l_flat)
+preds = Dense(6, activation='softmax')(l_dense)
+
+model = Model(sequence_input, preds)
+adadelta = optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
+lr_metric = get_lr_metric(adadelta)
+model.compile(loss='categorical_crossentropy',
+              optimizer=adadelta,
+              metrics=['acc'])
+
+tensorboard = callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=16, write_grads=True , write_graph=True)
+model_checkpoints = callbacks.ModelCheckpoint("checkpoint.h5", monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+lr_schedule = callbacks.LearningRateScheduler(initial_boost)
+
+model.summary()
+print("Training Progress:")
+history = model.fit(x_train, y_train, validation_data=(x_val, y_val),
+                    epochs=30, batch_size=64,
+                    callbacks=[lr_schedule])
+
+import pandas as pd
+pd.DataFrame(history.history).to_csv("new/history-"+str(filter_size)+".csv")
+model.save("ltsm-c-"+str(filter_size)+".h5")
